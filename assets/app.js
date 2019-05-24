68151f6ae685667a252be08670b209b74f6516e3
unknown <hannahuseva@gmail.com>
2019-05-24 17:06:01 -0500

    minor adj. working on comparing choices

--- assets/app.js
+++ assets/app.js
@@ -16,42 +16,60 @@ $(document).ready(function() {
     var comment = ""
     var yourWins=0
     var elseWins=0
+    var ties =0
     $("#click-button").on("click", function(event) {
         event.preventDefault()
         name = $("#name-input").val().trim()
         choice = $("input:radio[name=choice]:checked").val()
         comment = $("#comment-input").val().trim()
-        var yourchoice = $("<p>")
-        yourchoice.text(name + " chose " + choice)
-        $("#currentmatch").append(yourchoice)
-        var yourcomment = $("<p>")
-        yourcomment.text(name + " said " + comment)
-        $("#currentmatch").append(yourcomment)
+        //var yourchoice = $("<p>")
+        //yourchoice.text(name + " chose " + choice)
+        //$("#currentmatch").append(yourchoice)
+        //var yourcomment = $("<p>")
+        //yourcomment.text(name + " said " + comment)
+        //$("#currentmatch").append(yourcomment)
 
-        database.ref().push({
+        database.ref().set({
             name: name,
             choice: choice,
             comment: comment,
             yourWins: yourWins,
-            elseWins: elseWins
+            elseWins: elseWins,
+            ties: ties
         })  
     })
-    database.ref().on("child_added", function(childSnapshot) {
-        console.log(childSnapshot.val())
-        console.log(childSnapshot.val().name)
-        console.log(childSnapshot.val().choice)
-        var csv = childSnapshot.val()
+    database.ref().on("value", function(snapshot) {
+        console.log(snapshot.val())
+        console.log(snapshot.val().name)
+        console.log(snapshot.val().choice)
+        var csv = snapshot.val()
         var elsename = csv.name
         var elsechoice = csv.choice
         var elsecomment = csv.comment
-        var elsechoicedisp = $("<p>")
-        elsechoicedisp.text(elsename + " chose " + elsechoice)
-        $("#currentmatch").append(elsechoicedisp)
+        //var elsechoicedisp = $("<p>")
+        //elsechoicedisp.text(elsename + " chose " + elsechoice)
+       // $("#currentmatch").append(elsechoicedisp)
         var elsecommentDisp = $("<p>")
         elsecommentDisp.text(elsename + " said " + elsecomment)
         $("#currentmatch").append(elsecommentDisp)
 
 
+        if ((choice === "rock" && elsechoice === "scissors") ||
+          (choice  === "scissors" && elsechoice === "paper") || 
+          (choice  === "paper" && elsechoice === "rock")) {
+            yourWins++;
+        } else if (choice === elsechoice) {
+          ties++;
+        } else {
+          elseWins++;
+        }
+
+        $("#yourWins").text("Your wins: " + yourWins)
+        $("#elseWins").text("Somebody elses wins: " + elseWins)
+        $("#ties").text("Tied up: " + ties)
+        
+
+    //database.ref().remove()
 
     }, function(errorObject) {
         console.log("The read failed: " + errorObject.code)
